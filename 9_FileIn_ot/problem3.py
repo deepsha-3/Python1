b6f7ab14bbfe648fa713cbf7ce40ce97e7995ec6
deepsha-3 <itsmedeepsha375@gmail.com>
2025-06-19 14:21:55 +0545

    new files added

---
+++ 9_FileIn_ot/problem3.py
@@ -0,0 +1,13 @@
+# Write a program to generate multiplication tables from 2 to 20 and write it to the different files. Place these files in a folder for a 13-year old. 
+
+def generate_table(n):
+    table = " "
+    folder = "tables/"
+    for a in range(1, 11):
+        table +=(f"{n} x {a} = {n * a}\n")
+    with open(f"{folder}table_{n}.txt", "w") as file:
+        file.write(table)
+
+
+for a in range(2, 21):
+    generate_table(a)
---
+++ 9_FileIn_ot/problem5.py
@@ -0,0 +1,12 @@
+# Repeat program 4 for a list of such to be censored. 
+
+# donkey2.txt is a file which contains the word "Donkey" "Monkey" and "day". 
+
+words_to_replace= ["Donkey","Monkey", "day"]
+with open("D:\\python\\9_FileIn_ot\\donkey2.txt", "r") as file:
+    content = file.read()
+for word in words_to_replace:
+    content = content.replace(word, "#" * len(word))
+with open("D:\\python\\9_FileIn_ot\\donkey2.txt", "w") as file:
+    file.write(content)
+
