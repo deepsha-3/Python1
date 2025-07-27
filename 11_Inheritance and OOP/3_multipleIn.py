dfc98fb6e32180f6692781c8a4266371659fbecc
deepsha-3 <itsmedeepsha375@gmail.com>
2025-07-27 12:18:33 +0545

    new file

--- 11_Inheritance and OOP/3_multipleIn.py
+++ 11_Inheritance and OOP/3_multipleIn.py
@@ -1,2 +1,20 @@
 # multiple inheritance in python:
-     # A child clss inherits from more then one parent class.
+     # A child class inherits from more than one parent class.
+
+class Car:  # parent class 1
+    def start(self):
+        print("Car started")
+
+class Bike:  # parent class 2
+    def stop(self):
+            print("Bike stopped")
+
+class Vehicle(Car, Bike):  # child class from  both parents
+    def run(self):
+        print("Vehicle is running")
+
+
+v = Vehicle()  # object creation
+v.start()  # method call from Car class
+v.stop()   # method call from Bike class
+v.run()    # method call from Vehicle class
