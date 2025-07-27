d1dbd2c055eaa8b19029c866435036e177242b32
deepsha-3 <itsmedeepsha375@gmail.com>
2025-07-27 12:47:35 +0545

    new file

--- 11_Inheritance and OOP/3_multipleIn.py
+++ 11_Inheritance and OOP/3_multipleIn.py
@@ -3,18 +3,18 @@
 
 class Car:  # parent class 1
     def start(self):
-        print("Car started")
+        print("Car started.")
 
 class Bike:  # parent class 2
     def stop(self):
-            print("Bike stopped")
+            print("Bike stopped.")
 
 class Vehicle(Car, Bike):  # child class from  both parents
     def run(self):
-        print("Vehicle is running")
+        print("Vehicle is running.")
 
 
-v = Vehicle()  # object creation
-v.start()  # method call from Car class
-v.stop()   # method call from Bike class
-v.run()    # method call from Vehicle class
+obj = Vehicle()  # object creation
+obj.start()  # method call from Car class
+obj.stop()   # method call from Bike class
+obj.run()    # method call from Vehicle class
