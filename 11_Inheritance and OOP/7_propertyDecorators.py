2d4714664e6ebe16312d49acdd90cb199ecd6bb6
deepsha-3 <itsmedeepsha375@gmail.com>
2025-07-28 21:03:16 +0545

    update files

--- 11_Inheritance and OOP/7_propertyDecorators.py
+++ 11_Inheritance and OOP/7_propertyDecorators.py
@@ -1,2 +1,15 @@
 # property decorators in python:
-   #
+   # Property decorators are a way to manage the attributes of a class. They allow you to define methods in a class that can be accessed like attributes, providing a way to add validation or other logic when getting or setting a value.
+
+class Car:
+    model = "BMW X5"
+
+    def __init__(self, model):
+        self._model = model
+
+    @property
+    def model(self):
+         """Get the model of the car."""
+         return self._model
+
+obj = Car("BMW X5")
