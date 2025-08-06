a9a42f06e73dffe30c48032ff273ee899e0f2637
deepsha-3 <itsmedeepsha375@gmail.com>
2025-08-06 06:21:25 +0545

    update files

--- 9_FileIn_ot/1_file.py
+++ 9_FileIn_ot/1_file.py
@@ -1,6 +1,6 @@
 # text file 
 
 f=open(r"D:\python\9_FileIn_ot\file.txt") # opent is a buil-in function which helps in opening files. 
-content= f.read() # (r)read is used to reading our files 
+content= f.read()     # (r)read is used to reading our files 
 print(content)
-f.close()   # close is used to close our opened files 
+f.close()             # close is used to close our opened files 
