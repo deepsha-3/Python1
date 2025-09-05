42fd70089e2d6d05d898c66f0217b202feb4f08b
deepsha-3 <itsmedeepsha375@gmail.com>
2025-09-05 23:25:57 +0545

    new file

--- Basic Project/birthdaywish.py
+++ Basic Project/birthdaywish.py
@@ -8,7 +8,7 @@ from pygame import mixer
 
 # Adding music is optional as per your choice.
 mixer.init()
-mixer.music.load("")  # add your favorite music file name or path
+mixer.music.load("C:\Users\dell\Downloads\affordablewebsitesolutions.co.za - The Weeknd, Playboi Carti - Timeless (320 KBps).mp3")  # add your favorite music file name or path
 
 # sets background
 bg = turtle.Screen()
