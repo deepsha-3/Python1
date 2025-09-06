c678e41084d1d6b3b62fa0605c5398a8c76bb00d
deepsha-3 <itsmedeepsha375@gmail.com>
2025-09-06 15:08:24 +0545

    new file added

--- ICSC/problemB.py
+++ ICSC/problemB.py
@@ -1,1 +1,21 @@
 # Cake calculator in python
+
+# define a function 
+
+def cake_calculator(available_flour, available_sugar ):
+    # define the recipe requirements
+   
+   flour_per_cake = 100 # units
+   sugar_per_cake = 50  # units
+   maximum_cakes_by_flour = available_flour // flour_per_cake
+   maximum_cakes_by_sugar = available_sugar // sugar_per_cake
+
+
+# the actual number of cakes is limited by the ingredient in shortest supply
+   cakes_made = min(maximum_cakes_by_flour, maximum_cakes_by_sugar)
+
+# let's see what's left in the pantry
+   flour_left = available_flour - (cakes_made * flour_per_cake)
+   sugar_left = available_sugar - (cakes_made * sugar_per_cake)
+  
+   return [cakes_made, flour_left, sugar_left]
