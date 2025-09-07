3972e3d8c08b2a10175c7d65b96925ae61c284e0
deepsha-3 <itsmedeepsha375@gmail.com>
2025-09-07 13:07:05 +0545

    new file added

--- 1_starting/problem3.py
+++ 1_starting/problem3.py
@@ -2,5 +2,5 @@
 
 import pyttsx3
 engine = pyttsx3.init()
-engine.say("Hello, coders, Welcome to the world of Code!\n Your brain is hacked. \n   3\n  2\n   1")
+engine.say("Hello coders, Welcome to the world of Code!\n Your brain is hacked. \n   3\n  2\n   1")
 engine.runAndWait()
