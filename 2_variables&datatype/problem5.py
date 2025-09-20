1f4727d0c1590980b69118938b8f1b66219ad6b2
deepsha-3 <itsmedeepsha375@gmail.com>
2025-09-20 22:00:42 +0545

    new file added

--- 2_variables&datatype/problem5.py
+++ 2_variables&datatype/problem5.py
@@ -1,5 +1,5 @@
 # Write a python program to find an average of two numers entered by user. 
 
-a= int(input("Enter the first number: "))
-b= int(input("Enter the second number: "))
-print("The avereage of a and b is:", (a+b)/2)
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print("The average of a and b is:", (a+b)/2)
