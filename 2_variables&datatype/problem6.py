4cbf7d75381facd5d782833d79794c494d50e441
deepsha-3 <itsmedeepsha375@gmail.com>
2025-09-20 21:59:51 +0545

    new file added

--- 2_variables&datatype/problem6.py
+++ 2_variables&datatype/problem6.py
@@ -1,6 +1,6 @@
 # Write a python program to calculate the square of a numbers entered by the user. 
 
-a=int (input("Enter a number:"))
+a = int (input("Enter a number:"))
 print("The square of", a, "is", (a*a))
 
 
