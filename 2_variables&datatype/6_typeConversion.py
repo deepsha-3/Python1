99345308617a8c5ae7c81dd13f3c1e1552aef89c
deepsha-3 <itsmedeepsha375@gmail.com>
2025-09-21 23:33:05 +0545

    new file added

--- 2_variables&datatype/6_typeConversion.py
+++ 2_variables&datatype/6_typeConversion.py
@@ -1,6 +1,6 @@
 # Convert string to float 
 
-a="12.3"
-b=float(a)  # Convert string to float
-t= type(b)
+a = "12.3"
+b = float(a)  # Convert string to float
+t = type(b)
 print(t)  # Output: <class 'float'>
