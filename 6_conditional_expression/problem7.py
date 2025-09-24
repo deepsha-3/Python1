ec5e8db9b4423643041985cb9fb8e66f8f0f295a
deepsha-3 <itsmedeepsha375@gmail.com>
2025-09-24 20:57:16 +0545

    new file added

--- 6_conditional_expression/problem7.py
+++ 6_conditional_expression/problem7.py
@@ -19,8 +19,8 @@ elif(marks < 90 and marks >= 80):
 elif(marks < 80 and marks >= 70):
     grade = "B"  
 
-elif(marks<70 and marks>=60):
-    grade="C"
+elif(marks < 70 and marks >= 60):
+    grade = "C"
 
 elif(marks<60 and marks>=50):
     grade="D"
