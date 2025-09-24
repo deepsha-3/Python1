f2e6fed7671f45042e9e89f299a0c693eab7d9bc
deepsha-3 <itsmedeepsha375@gmail.com>
2025-09-24 20:55:22 +0545

    new file added

--- 6_conditional_expression/problem7.py
+++ 6_conditional_expression/problem7.py
@@ -13,7 +13,7 @@ marks=int(input("Enter your marks:"))
 if(marks <= 100 and marks >= 90):
     grade = "EX"
 
-elif(marks<90 and marks>=80):
+elif(marks < 90 and marks >= 80):
     grade="A"
 
 elif(marks<80 and marks>=70):
