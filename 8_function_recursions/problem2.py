ad106ea37a3ca534859fa5f55ba04a9e0c1894ec
deepsha-3 <itsmedeepsha375@gmail.com>
2025-09-26 20:47:32 +0545

    new file added

---
+++ 8_function_recursions/problem2.py
@@ -0,0 +1,33 @@
+# Write a program using functions to find greatest of three numbers.
+
+# input from user
+def greatest(d,e, f):
+    if(d>e and d>f):
+        return d
+    elif(e>d and e>f):
+        return e
+    elif(f>e and f>d):
+        return f
+    
+d= int(input("Enter the first number: "))
+e= int(input("Enter the second number: "))
+f= int(input("Enter the third number: "))
+
+print(greatest(d,e,f))
+
+
+
+# provide input
+def greatest(d,e, f):
+    if(d>e and d>f):
+        return d
+    elif(e>d and e>f):
+        return e
+    elif(f>e and f>d):
+        return f
+    
+d= 9
+e= 6
+f= 4
+
+print(greatest(d,e,f))
