4ba4777f3547ceb40d9506d6a121c668eee79427
deepsha-3 <itsmedeepsha375@gmail.com>
2025-09-26 20:55:53 +0545

    new file added

--- 8_function_recursions/problem1.py
+++ 8_function_recursions/problem1.py
@@ -1,6 +1,6 @@
 # Write a program to greet a user with "have a good day" using functions.
 
-name=(input("Enter your name:"))
+name = (input("Enter your name:"))
 
 def day():
     print(f"{name},have a good day")
