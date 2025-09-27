f458a5ef44fd2fceb3c136b43452c0f8501bd341
deepsha-3 <itsmedeepsha375@gmail.com>
2025-09-27 22:23:46 +0545

    new file added

---
+++ 8_function_recursions/problem4.py
@@ -0,0 +1,6 @@
+# How do you prevent a python print() function to print a new line at the end. 
+
+print("Dipisha")
+print("Abel")
+print("Levet", end="")  # end = "" helps to prevent a new line.
+print("Deepsha", end="")
