daf5a3364c8f23c871c32fd0b81c7bab5a63aaea
deepsha-3 <itsmedeepsha375@gmail.com>
2025-09-27 22:35:49 +0545

    new file added

--- 8_function_recursions/problem7.py
+++ 8_function_recursions/problem7.py
@@ -1,7 +1,7 @@
 # Write a python function which converst inches to cms.
 
 def fun(inch):
-    return inch*2.54
- 
+    return inch * 2.54
+
 num= int(input("Enter the value in inches:"))
 print("The value of in cms is", + fun(num))
