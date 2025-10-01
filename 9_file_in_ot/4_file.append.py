56641c8c1736a1f6012623bcf6d3404a32f2dfeb
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-01 20:55:08 +0545

    new file added

--- 9_file_in_ot/4_file.append.py
+++ 9_file_in_ot/4_file.append.py
@@ -1,7 +1,7 @@
 # file append mode
 string = "Hello, coders It's me Dipisha Dumre."
 
-f=open("mywritefile.txt", "a")  # when we use append in our code it will be added same text in the inside of txt file.
+f = open("mywritefile.txt", "a")  # when we use append in our code it will be added same text in the inside of txt file.
 f.write(string)
 f.close()
 
