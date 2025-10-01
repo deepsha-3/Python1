87dc0b987b93c7bfb99fff7e480b09b352c373a3
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-01 20:54:33 +0545

    new file added

--- 9_file_in_ot/4_file.append.py
+++ 9_file_in_ot/4_file.append.py
@@ -1,5 +1,5 @@
 # file append mode
-string= "Hello, coders It's me Dipisha Dumre."
+string = "Hello, coders It's me Dipisha Dumre."
 
 f=open("mywritefile.txt", "a")  # when we use append in our code it will be added same text in the inside of txt file.
 f.write(string)
