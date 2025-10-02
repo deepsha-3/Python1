5bc29af4cc35f4470b8144cd18349a3fead16dfa
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-02 22:43:37 +0545

    new file added

--- 9_file_in_ot/problem1.py
+++ 9_file_in_ot/problem1.py
@@ -1,7 +1,7 @@
 # Write a program to read the text from givien file 'poems.txt' and find out whether it constains the word 'twinkle'. 
 
 f = open("D:\\python\\9_FileIn_ot\\poems.txt", 'r')
-text=f.read()
+text= f.read()
 if("twinkle" in text):
     print(" Oh yes, The word 'twinkle' is present in the poems.txt file.")
 else:
