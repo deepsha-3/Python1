b243588ee7e2f8091c1762e410a64ee4d077116b
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-03 21:47:39 +0545

    new file added

--- 9_file_in_ot/problem3.py
+++ 9_file_in_ot/problem3.py
@@ -1,7 +1,7 @@
 # Write a program to generate multiplication tables from 2 to 20 and write it to the different files. Place these files in a folder for a 13-year old. 
 
 def generate_table(n):
-    table =" "
+    table = " "
     folder ="tables/"
     for a in range(1, 11):
         table +=(f"{n} x {a} = {n * a}\n")
