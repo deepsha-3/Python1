29b340bc909702dc3957af9b82cdd36735dcdf22
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-04 12:51:44 +0545

    new file

--- 9_file_in_ot/problem4.py
+++ 9_file_in_ot/problem4.py
@@ -1,7 +1,7 @@
 # A file contians a word "Donkey" multiple times. You need to write a proram which replace this word with "#####" by updating the same file. 
 
 #  donkey.txt is a file which contains the word "Donkey" multiple times.
-word_to_replace ="Donkey"
+word_to_replace = "Donkey"
 with open("D:\\python\\9_FileIn_ot\\donkey.txt", "r") as file:
     content=file.read()
 content=content.replace(word_to_replace, "#####")
