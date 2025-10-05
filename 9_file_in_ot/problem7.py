904f93bf489e2d5b923685d192d111dbbe72c8ce
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-06 02:06:15 +0545

    new file

--- 9_file_in_ot/problem7.py
+++ 9_file_in_ot/problem7.py
@@ -2,16 +2,16 @@
 
 
 with open("D:\\python\\9_FileIn_ot\\logfile.txt", "r") as file:
-    content = file.read()
+    content=file.read()
 
-line_num = 1
-found = False
+line_num=1
+found=False
 for line in content.splitlines():
     if "python" in line.lower():
         print(f"Python is available in logfile.txt. Line number: {line_num}")
-        found = True
+        found=True
         break
-    line_num += 1
+    line_num +=1
 
 if not found:
     print("Python is not available in logfile.txt.")
