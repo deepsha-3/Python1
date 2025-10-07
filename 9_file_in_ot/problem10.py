ee1377cb5d8255906e898cf0b28bc8ee02d18581
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-08 01:52:59 +0545

    new file

--- 9_file_in_ot/problem10.py
+++ 9_file_in_ot/problem10.py
@@ -1,4 +1,4 @@
 # Write a program to wipe out the content a file using python. 
 
 with open("D:\\python\\9_FileIn_ot\\this_copy.txt", "w") as file:
-    file.write("")  # This will clear the content of the file 'this_copy.txt'.
+    file.write("")        # This will clear the content of the file 'this_copy.txt'.
