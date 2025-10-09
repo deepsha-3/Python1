b41ffc7ecd54b6a9b6e2ea0861bd3550fdf054d3
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-09 23:42:53 +0545

    new file

---
+++ 3_String/2_string_slicing.py
@@ -0,0 +1,18 @@
+# String slcing is a way to access a part of the string using indexing.
+
+# String slicing is a way to access a part of the string using indexing.
+
+# String slicing is done using the colon (:) operator.
+
+name = "Dipisha"
+
+name_slice = name[0:3]  # Accessing characters from index 0 to 2
+print(name_slice)  # Output: Dip
+
+
+name_slice = name[3:]   # Accessing characters from index 3 to the end
+print(name_slice)  # Output: sha
+
+
+name_slice = name[-4:-1]  # Accessing characters from index -4 to -2 (not including -1)
+print(name_slice)  # Output: pis
