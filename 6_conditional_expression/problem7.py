34d13c6813870d3f87bd58b495b3cc37ba35e7ce
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-09 23:29:17 +0545

    new file

--- 6_conditional_expression/problem7.py
+++ 6_conditional_expression/problem7.py
@@ -1,10 +1,10 @@
 # Write a program to calculate the grade of student from his marks from the following scheme:
 """
-90-100 => Ex
-80-90 => A
-70-80 => B 
-60-70 => C 
-50-60 => D 
+90 - 100 => Ex
+80 - 90 => A
+70 - 80 =>  B 
+60 - 70 => C 
+50 - 60 => D 
 < 50 => E
 """
 
@@ -26,6 +26,6 @@ elif(marks < 60 and marks >= 50):
     grade = "D"
 
 elif(marks < 50):
-    grade = "E"
+    grade ="E"
 
 print("Your grade is:", grade)
