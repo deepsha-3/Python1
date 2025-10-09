4eb47d05c0b951edeea98486eef07c9fc6316654
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-09 23:46:38 +0545

    new file

---
+++ 3_String/3_scling_skip.py
@@ -0,0 +1,7 @@
+# String slicing with skips is a way to access characters in a string while skipping certain characters based on a specified step.
+
+# String slicing with skips is done using the colon (:) operator along with a step value.
+
+name = "Dipisha" 
+name_slice = name[0:7:3]  # Accessing characters from index 0 to 6 with a step of 2 
+print(name_slice)  # Output: Dpa
