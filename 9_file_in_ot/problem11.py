e2a684e4320ee911a2e531c38ce59ebb82e170b5
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-09 23:24:55 +0545

    new file

--- 9_file_in_ot/problem11.py
+++ 9_file_in_ot/problem11.py
@@ -1,7 +1,7 @@
 # Wite a python program to rename a file to "renamed_by_python.txt".
 
 with open("D:\\python\\9_FileIn_ot\\thisnew.txt", "r") as file:
-    content=file.read()   
+    content= file.read()   
 
 with open("D:\\python\\9_FileIn_ot\\renamed_by_python.txt", "w") as file:
     file.write(content)
