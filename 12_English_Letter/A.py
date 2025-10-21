e2ef9201ce0e8e15e6140d90abcfac9097e9626e
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-21 21:15:36 +0545

    new file

---
+++ 12_English_Letter/A.py
@@ -0,0 +1,14 @@
+
+# 
+
+n = 4 
+
+for i in range(n):
+    for j in range(2 * n - 1):
+        if j == n - i - 1 or j == n + i - 1:
+            print("*", end="")
+        elif i == n // 2 and n - i - 1 < j < n + i - 1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
