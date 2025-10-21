ba3066e10b42563ba0e1c24848816038a036c4b8
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-21 21:20:41 +0545

    new file

--- 12_English_Letter/B.py
+++ 12_English_Letter/B.py
@@ -1,2 +1,30 @@
 
 # creating the letter 'B' pattern
+
+n = 7  # Height of the B
+
+for i in range(n):
+    for j in range(n):
+        if j == 0:
+            print("*", end="")  # Left spine
+        elif (i == 0 or i == n // 2 or i == n - 1) and j < n - 1:
+            print("*", end="")  # Top, middle, bottom bars
+        elif (i < n // 2 or i > n // 2) and j == n - 1:
+            print("*", end="")  # Right curve edges
+        else:
+            print(" ", end="")
+    print()
+
+
+
+
+
+""" n= 5
+for i in range(n,-1,-1):                      
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+for i in range(1,n+1):
+    for j in range(i+1):
+        print("*",end=" ")
+    print() """
