b0f137ce7adb99fac11f522f6822719a07447d7a
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-22 22:57:48 +0545

    new file

--- 12_English_Letter/C.py
+++ 12_English_Letter/C.py
@@ -6,7 +6,7 @@ for i in range(n):
     for j in range(n):
         if (i == 0 or i == n - 1) and j > 0:  # Top and bottom horizontal lines
             print("*", end="")
-        elif (i > 0 and i < n - 1) and j == 0:  # Left ve
+        elif (i > 0 and i < n - 1) and j == 0:  
             print("*", end="")
         else:
             print(" ", end="")
