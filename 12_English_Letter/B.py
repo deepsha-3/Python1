31a65df8fac439384aa748d1a3a09205668a524c
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-22 22:49:51 +0545

    new file

--- 12_English_Letter/B.py
+++ 12_English_Letter/B.py
@@ -6,7 +6,7 @@ n = 7  # Height of the B
 for i in range(n):
     for j in range(n):
         if j == 0:
-            print("*", end="")  # Left spine
+            print("*", end="")  # Left
         elif (i == 0 or i == n // 2 or i == n - 1) and j < n - 1:
             print("*", end="")  
         elif (i < n // 2 or i > n // 2) and j == n - 1:
