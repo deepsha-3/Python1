354f0d19746defdacd67c0606ebc8064824254a6
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-23 21:37:38 +0545

    new file

--- 12_English_Letter/D.py
+++ 12_English_Letter/D.py
@@ -5,7 +5,7 @@ n = 7
 
 for i in range(n):
     for j in range(n):
-        if j == 0:
+    
             print("*", end="")  
         elif (i == 0 or i == n - 1) and j < n - 1:
             print("*", end="")  
