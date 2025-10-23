9ffa9182ec17909fb66e940835ff2ffabb218856
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-23 21:23:21 +0545

    new file

--- 12_English_Letter/C.py
+++ 12_English_Letter/C.py
@@ -4,7 +4,7 @@ n = 7
 
 for i in range(n):
     for j in range(n):
-        if (i == 0 or i == n - 1) and j > 0:  # Top 
+        if (i == 0 or i == n - 1) and j > 0:  
             print("*", end="")
         elif (i > 0 and i < n - 1) and j == 0:  
             print("*", end="")
