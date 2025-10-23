39fc07f202778dcf49575eafcc174a13b7b3732c
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-23 21:34:41 +0545

    new file

--- 12_English_Letter/D.py
+++ 12_English_Letter/D.py
@@ -2,3 +2,16 @@
 # creating the letter 'D' pattern
 
 n = 7
+
+for i in range(n):
+    for j in range(n):
+        if j == 0:
+            print("*", end="")  
+        elif (i == 0 or i == n - 1) and j < n - 1:
+            print("*", end="")  
+        elif (i > 0 and i < n - 1) and j == n - 1:
+            print("*", end="")  
+        else:
+            print(" ", end="")
+  
+  
