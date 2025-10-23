c94ead113c577e5916378fb2c8f87deb0fe22ec4
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-23 21:40:04 +0545

    new file

--- 12_English_Letter/D.py
+++ 12_English_Letter/D.py
@@ -11,8 +11,7 @@ for i in range(n):
             print("*", end="")  
         elif (i > 0 and i < n - 1) and j == n - 1:
             print("*", end="")  
-        else:
-            print(" ", end="")
+      
     print()
     
   
