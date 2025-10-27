02743390d7cf8ef8ffb133a5ac99a91ed0716c48
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-27 22:10:19 +0545

    new file

--- 12_English_Letter/G.py
+++ 12_English_Letter/G.py
@@ -4,3 +4,6 @@
 n = 7
 
 for i in range(n):
+    for j in range(n):
+        if (j == 0 and i != 0 and i != n - 1) or (i == 0 and j != 0 and j != n - 1) or (i == n - 1 and j != 0 and j >= n // 2) or (j == n - 1 and i >= n // 2 and i != n - 1) or (i == n // 2 and j >= n // 2):
+          
