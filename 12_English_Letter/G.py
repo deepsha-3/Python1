fcb36e5b9818027e0df6ea7b71e259f23e97e8d4
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-27 22:06:05 +0545

    new file

--- 12_English_Letter/G.py
+++ 12_English_Letter/G.py
@@ -8,3 +8,4 @@ for i in range(n):
         if (j == 0) or (j == n-1) or (i == j and i <= n//2) or (i + j == n - 1 and i <= n//2):
             print("*", end="")
         else:
+            print(" ", end="")
