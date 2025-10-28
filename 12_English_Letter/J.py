2aa4f0252b3e46a2989b6b73a4a55a824222d8fa
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-28 20:37:05 +0545

    new file

--- 12_English_Letter/J.py
+++ 12_English_Letter/J.py
@@ -5,4 +5,4 @@ num = 7
 for i in range(num):
     for p in range(num):
         if (i == 0) or (p == num // 2 and i != num -1 ) or (i == num -1 and p <= num // 2) :
-           
+           print("*", end=" ")
