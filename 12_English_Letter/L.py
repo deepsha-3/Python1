1076dc0bd341d0544d21078d4e511d1fc42bc066
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-30 23:24:16 +0545

    new file

--- 12_English_Letter/L.py
+++ 12_English_Letter/L.py
@@ -6,5 +6,6 @@ number = 7
 for i in range(number):
     for j in range(number):
         if j == 0 or i == number - 1:
-           print("*", end="")
+             print("*", end="")
         else:
+              
