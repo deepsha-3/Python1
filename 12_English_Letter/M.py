3b109b52e732bd955e0e66b117fc68dc990eb3d1
deepsha-3 <itsmedeepsha375@gmail.com>
2025-10-31 21:04:30 +0545

    new file

--- 12_English_Letter/M.py
+++ 12_English_Letter/M.py
@@ -6,3 +6,4 @@ num = 8
 for i in range(num):
     for j in range(num):
         if j == 0 or j == num -1 or (i == j and i<= num//2) or (i + j == num -1 and i <= num//2):
+            print("*", end="")
