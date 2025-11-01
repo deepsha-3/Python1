397508a96c00e7ec0d26292ccbf021bf9a65fe2c
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-01 09:03:47 +0545

    new file

--- 12_English_Letter/O.py
+++ 12_English_Letter/O.py
@@ -6,3 +6,4 @@ num = 7
 for i in range(num):
     for j in range (num):
         if (i == 0 and j != 0 and j != num -1 ) or (i == num -1 and j != 0 and j != num -1 ) or (j == 0 and i != 0 and i != num -1 ) or (j == num -1 and i != 0 and i ! = num -1 ):
+            print("*", end="")
