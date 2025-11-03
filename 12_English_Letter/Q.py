3cd3f59ca296d6569896d122aa1cca36cd7be53e
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-03 11:06:21 +0545

    new file

--- 12_English_Letter/Q.py
+++ 12_English_Letter/Q.py
@@ -5,4 +5,4 @@ num = 7
 
 for i in range(num):
     for j in range(num):
-       
+       if (j == 0 and i != num - 1) or (i == 0 and j != num - 1) or (i == num - 1 and j != 0 and j != num - 1) or (j == num - 1 and i != 0 and i != num - 1) or (i == j and i >= num // 2):
