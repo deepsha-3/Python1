c239883124e36c79699f5323eacaafd5eddfc3dd
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-04 20:52:26 +0545

    new file

--- 12_English_Letter/R.py
+++ 12_English_Letter/R.py
@@ -4,7 +4,7 @@
 num = 7
 
 for i in range(num):
-    for j in range(5):
+    for j in range(num):
         if (j == 0) or (i == 0 and j < 4) or (i == 3 and j < 4) or (j == 4 and i != 0 and i < 3) or (i - j == 3):
             print("*", end="")
         else:
