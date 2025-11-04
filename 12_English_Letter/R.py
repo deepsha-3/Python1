a91cca6561d47e45b2d9ed972f87cf0dd2706db5
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-04 20:51:41 +0545

    new file

--- 12_English_Letter/R.py
+++ 12_English_Letter/R.py
@@ -1,8 +1,9 @@
 
 # letter pattern R
 
+num = 7
 
-for i in range(7):
+for i in range(num):
     for j in range(5):
         if (j == 0) or (i == 0 and j < 4) or (i == 3 and j < 4) or (j == 4 and i != 0 and i < 3) or (i - j == 3):
             print("*", end="")
