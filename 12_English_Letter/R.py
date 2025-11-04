534a580517da36bae57e3e1830779d8ac5c8cd55
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-04 20:46:16 +0545

    new file

--- 12_English_Letter/R.py
+++ 12_English_Letter/R.py
@@ -6,3 +6,4 @@ for i in range(7):
         if (j == 0) or (i == 0 and j < 4) or (i == 3 and j < 4) or (j == 4 and i != 0 and i < 3) or (i - j == 3):
             print("*", end="")
         else:
+            print(" ", end="")
