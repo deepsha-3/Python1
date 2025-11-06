7de5ce7be9a8b540b40b18844c22e4a552650aba
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-06 23:55:12 +0545

    new file added

--- 12_English_Letter/U.py
+++ 12_English_Letter/U.py
@@ -4,3 +4,6 @@
 for i in range(7):
     for j in range(7):
        if ((j == 0 or j == 6) and i != 6) or (i == 6 and j != 0 and j != 6):
+              print("*", end="")
+        else:
+            
