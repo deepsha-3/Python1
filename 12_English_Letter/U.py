f89531338c7765a43c3675f4505542049774275e
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-06 23:55:45 +0545

    new file

--- 12_English_Letter/U.py
+++ 12_English_Letter/U.py
@@ -6,4 +6,4 @@ for i in range(7):
        if ((j == 0 or j == 6) and i != 6) or (i == 6 and j != 0 and j != 6):
               print("*", end="")
         else:
-            
+               print(" ", end="")
