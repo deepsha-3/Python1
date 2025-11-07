9a3ab61384670ceb4cd5030c683dab9cf39cfa25
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-07 22:50:04 +0545

    new file

--- 12_English_Letter/V.py
+++ 12_English_Letter/V.py
@@ -5,4 +5,5 @@ num = 7
 
 for i in range(num):
     for j in range(num):
-        
+        if (j == i and i < num // 2) or (j == num - i - 1 and i < num // 2) or (i - j == num // 2 and i >= num // 2) or (i + j == num + (num // 2) - 1 and i >= num // 2):
+           
