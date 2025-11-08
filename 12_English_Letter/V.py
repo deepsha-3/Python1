74666764b34974ff6d6573cf9fbee60472e52c16
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-08 22:29:12 +0545

    new file

--- 12_English_Letter/V.py
+++ 12_English_Letter/V.py
@@ -4,7 +4,7 @@
 num = 7
 
 for i in range(num):
-    for j in range(num * 2 - 1):  # wider grid to allow symmetry
+    for j in range(num * 2 - 1): 
         if j == i or j == (num * 2 - 2 - i):
             print("*", end="")
         else:
