df2057e6941ea633e83516cf9a838d424a5453ca
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-08 22:50:29 +0545

    new code file

--- 12_English_Letter/W.py
+++ 12_English_Letter/W.py
@@ -5,4 +5,7 @@ num = 7
 
 for i in range(num):
     for j in range(num * 4 - 3): 
-        
+        if j == i or j == (num * 2 - 2 - i) or j == (num * 2 - 2 + i) or j == (num * 4 - 4 - i):
+            print("*", end="")
+        else:
+            print(" ", end="")
