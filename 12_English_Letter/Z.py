a38ef7927fa9e9b2cb9b52935d406e20a81e5b69
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-12 22:15:34 +0545

    new file

--- 12_English_Letter/Z.py
+++ 12_English_Letter/Z.py
@@ -7,4 +7,5 @@ for i in range(n):
     for j in range(n * 2 - 1):
         if i == 0 or i == n - 1:
             print("*", end="")
-       
+        elif j == n - 1 - i:
+            print("*", end="")
