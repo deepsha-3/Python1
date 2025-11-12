3668af98f8a647d0f45aeead59de765ac94f3a26
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-12 22:22:50 +0545

    new file added

--- 12_English_Letter/Z.py
+++ 12_English_Letter/Z.py
@@ -4,12 +4,11 @@
 n = 7
 
 for i in range(n):
-    for j in range(n * 2 - 1):
+    for j in range(n * 2 - 1):   # <-- problem here
         if i == 0 or i == n - 1:
             print("*", end="")
         elif j == n - 1 - i:
             print("*", end="")
         else:
             print(" ", end="")
-
     print()
