21924b5da20dd491688e47ff5e8859e3acb89b09
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-12 22:08:18 +0545

    new file

--- 12_English_Letter/Y.py
+++ 12_English_Letter/Y.py
@@ -7,4 +7,7 @@ for i in range(n):
     for j in range(n * 2 - 1):
         if j == n // 2:
             print("*", end="")
+        elif i >= n // 2 and (j == i - n // 2 or j == (n * 2 - 2 - (i - n // 2))):
+            print("*", end="")
+            
         
