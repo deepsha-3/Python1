cc303a32756b8452ed3eca0a70d43ae683805ccf
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-12 22:24:56 +0545

    new file add

--- 12_English_Letter/Z.py
+++ 12_English_Letter/Z.py
@@ -1,10 +1,9 @@
 
-# letter Pattern Z
 
 n = 7
 
 for i in range(n):
-    for j in range(n * 2 - 1):   # <-- problem here
+    for j in range(n):
         if i == 0 or i == n - 1:
             print("*", end="")
         elif j == n - 1 - i:
