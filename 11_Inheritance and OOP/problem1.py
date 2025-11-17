94752f5d014ea704b0f9969dd6f57abb9d5889cd
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-17 22:11:06 +0545

    new file added

--- 11_Inheritance and OOP/problem1.py
+++ 11_Inheritance and OOP/problem1.py
@@ -6,10 +6,18 @@ class Vector2D:
         self.a = a
         self.b = b
     
-class Vector3D:
+    def display(self):
+        print(f"Vector2D: ({self.a}, {self.b})")
+    
+class Vector3D(Vector2D):
     def __init__(self, a, b, c):
         super().__init__(a, b)
         self.c = c
+
+    def display(self):
+        print(f"Vector3D: ({self.a}, {self.b}, {self.c})")
     
 obj1 = Vector2D(3, 4)
+obj1.display()
 obj2 = Vector3D(3, 4, 5)
+obj2.display()
