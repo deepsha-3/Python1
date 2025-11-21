6f12849b731f184aee3a902615ca5caacda69f53
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-21 08:38:25 +0545

    new file

--- 11_Inheritance and OOP/problem4.py
+++ 11_Inheritance and OOP/problem4.py
@@ -5,3 +5,7 @@ class Complex:
     def __init__(self, real, imag):
         self.real = real
         self.imag = imag
+    
+    def __add__(self, other):
+       # return Complex(self.real + other.real, self.imag + other.imag)
+ 
