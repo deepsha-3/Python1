58f8ff0aee01474ae1dff4c704863e998af21a36
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-21 08:36:52 +0545

    new file

--- 11_Inheritance and OOP/problem4.py
+++ 11_Inheritance and OOP/problem4.py
@@ -2,3 +2,4 @@
 # Write a cclass "Complex" to represent complex numbers (real and imaginary parts), along with overloaded operators '+' and '*' which adds and multiplies them. 
 
 class Complex:
+    def __init__(self, real, imag):
