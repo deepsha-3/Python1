7af989dd536e77e95b4be9e82a4e10462c0a2b06
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-22 23:35:33 +0545

    new file

--- 11_Inheritance and OOP/problem5.py
+++ 11_Inheritance and OOP/problem5.py
@@ -1,2 +1,2 @@
 
-# Write a class vector representing a vector of n dimensions. overloaded the +  and * operator which calcuates the sum and 
+# Write a class vector representing a vector of n dimensions. overloaded the +  and * operator which calcuates the sum and the dot(.) 
