11c97542516d31a6761013f267482e0b0af486e1
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-23 17:55:24 +0545

    new file

--- 11_Inheritance and OOP/problem5.py
+++ 11_Inheritance and OOP/problem5.py
@@ -1,7 +1,7 @@
 
 # Write a class vector representing a vector of n dimensions. overloaded the +  and * operator which calcuates the sum and the dot(.) product of them. 
 
-from unittest import result
+
 
 
 class Vector:
