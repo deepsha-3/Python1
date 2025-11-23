2a0b4d1a359a01f42b85770099229edc089cbdb7
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-23 18:07:23 +0545

    new file

--- 11_Inheritance and OOP/problem5.py
+++ 11_Inheritance and OOP/problem5.py
@@ -1,9 +1,6 @@
 
 # Write a class vector representing a vector of n dimensions. overloaded the +  and * operator which calcuates the sum and the dot(.) product of them. 
 
-
-
-
 class Vector:
     def __init__(self, a, b, c):
         self.a = a
@@ -12,23 +9,21 @@ class Vector:
     
     def __add__(self, other):
         return Vector(self.a + other.a, self.b + other.b, self.c + other.c)
-        
     
     def __mul__(self, other):
         return self.a * other.a + self.b * other.b + self.c * other.c
-       
     
     def __str__(self):
         return f"({self.a}, {self.b}, {self.c})"
-    
-    v1 = Vector(1, 2, 3)
-    v2 = Vector(4, 5, 6)
-    v3 = Vector(7, 8, 9)
 
-    print("Sum of v1 and v2:", v1 + v2)
-    print("Dot product of v2 and v3:", v2 * v3)
+v1 = Vector(1, 2, 3)
+v2 = Vector(4, 5, 6)
+v3 = Vector(7, 8, 9)
 
-    print(v1 + v3)
-    print(v1 * v3)
+print("Sum of v1 and v2:", v1 + v2)
+print("Dot product of v2 and v3:", v2 * v3)
 
-    # u = Vector(5, 6, 7)
+print(v1 + v3)
+print(v1 * v3)
+
+# u = Vector(5, 6, 7)
