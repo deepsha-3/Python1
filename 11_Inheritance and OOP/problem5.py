49e640c52397e320c5778304be31af885382f2b6
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-23 17:42:04 +0545

    new file

--- 11_Inheritance and OOP/problem5.py
+++ 11_Inheritance and OOP/problem5.py
@@ -20,3 +20,15 @@ class Vector:
     
     def __str__(self):
         return f"({self.a}, {self.b}, {self.c})"
+    
+    v1 = Vector(1, 2, 3)
+    v2 = Vector(4, 5, 6)
+    v3 = Vector(7, 8, 9)
+
+    print("Sum of v1 and v2:", v1 + v2)
+    print("Dot product of v2 and v3:", v2 * v3)
+
+    print(v1 + v3)
+    print(v1 * v3)
+
+    # u = Vector(5, 6, 7)
