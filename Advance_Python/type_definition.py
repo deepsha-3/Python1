5ca258dd99eb92ade54db7e3bf7f9522f19af88b
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-26 22:10:45 +0545

    new code

--- Advance_Python/type_definition.py
+++ Advance_Python/type_definition.py
@@ -1,2 +1,4 @@
 
 # Type definitions in Python allow you to specify the expected data types of variables, function parameters, and return values.
+
+# This helps improve code readability and can catch type-related errors during development.
