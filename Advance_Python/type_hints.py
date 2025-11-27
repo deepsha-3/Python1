cf9ca45d844eaf59b040e7077330531208dfe1ca
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-27 22:03:47 +0545

    new file

--- Advance_Python/type_hints.py
+++ Advance_Python/type_hints.py
@@ -5,3 +5,6 @@ from typing import List, Dict, Tuple, Optional, Union
 
 # List type hint
 numbers: List[int] = [1, 2, 3, 4 ]
+
+# Dictinonary type hint
+user_info: Dict[str, int] = {"name": "Karan", "age": 25}
