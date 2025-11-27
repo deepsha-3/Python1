65709323f0d827e8a7c3912c538575ef27a0fe88
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-27 22:09:10 +0545

    new file

--- Advance_Python/type_hints.py
+++ Advance_Python/type_hints.py
@@ -11,3 +11,8 @@ user_info: Dict[str, int] = {"name": "Karan", "age": 25}
 
 # Tuple type hint
 canditaes : Tuple[str, int ] = ("Partiyush", 24)
+
+# Optional type hint
+def get_user_age(user: Dict[str, Union[str, int]]) -> Optional[int]:
+    return user.get("age")          
+print(get_user_age(user_info))  
