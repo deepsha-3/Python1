578273694f0748d79dbf181eb98603aedeae6a7d
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-30 23:28:55 +0545

    new file

--- Advance_Python/match_case.py
+++ Advance_Python/match_case.py
@@ -16,9 +16,7 @@ def weekday_name(day_number):
             return "Thursday"
        case 6:
             return "Friday"
-       case 7:
-            return "Saturday"
-       
+      
 
 if __name__ == "__main__":
     for i in range(1, 9):
