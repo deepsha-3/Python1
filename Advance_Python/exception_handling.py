af4dd3f0513d1b85124042a17f382d05a9f8b29a
deepsha-3 <itsmedeepsha375@gmail.com>
2025-12-01 23:09:31 +0545

    new file

--- Advance_Python/exception_handling.py
+++ Advance_Python/exception_handling.py
@@ -2,4 +2,8 @@
 # exception handling in python
 
 try:
-  num = int(input("Enter a number: "))
+    num = int(input("Enter a number: "))
+    print("You entered:", num)
+
+except Exception as e:
+    print("An error occurred:", e)
