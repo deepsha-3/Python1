08bac2b6240c5c594e4d6c677d87249b40f38be1
deepsha-3 <itsmedeepsha375@gmail.com>
2025-12-03 00:15:01 +0545

    new file

--- Advance_Python/raising_exception.py
+++ Advance_Python/raising_exception.py
@@ -5,4 +5,5 @@ a = int(input("Enter a first number: "))
 b = int(input("Enter a second number: "))
 
 if b == 0:
+    raise ValueError("The denominator cannot be zero.")
 print(f"The division of two numbers is:  {a/b}")
