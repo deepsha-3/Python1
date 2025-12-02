5ea869d343974e7777e466e73a8baf37fa42039f
deepsha-3 <itsmedeepsha375@gmail.com>
2025-12-03 00:13:09 +0545

    new file

--- Advance_Python/raising_exception.py
+++ Advance_Python/raising_exception.py
@@ -1,7 +1,7 @@
 
 # raising an exception in Python
 
-a = (input("Enter a first number: "))
+a = int(input("Enter a first number: "))
 b = (input("Enter a second number: "))
 
 print(f"The division of two numbers is:  {a/b}")
