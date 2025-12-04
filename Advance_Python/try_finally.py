7da8821be2c1df430cb44a4b5c67a4a05283d929
deepsha-3 <itsmedeepsha375@gmail.com>
2025-12-04 22:44:52 +0545

    new wfile

--- Advance_Python/try_finally.py
+++ Advance_Python/try_finally.py
@@ -5,3 +5,6 @@ try:
     num1 = int(input("Enter a number: "))
     num2 = int(input("Enter second number: "))
     result = num1 / num2
+
+except Exception as e:
+    print("An error occurred:", e)
