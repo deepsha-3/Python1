3dd060ab4bb1791fd0c7850476aa919576870c5e
deepsha-3 <itsmedeepsha375@gmail.com>
2025-12-06 22:01:08 +0545

    added file

---
+++ 13_Advance_Python/try_finally.py
@@ -0,0 +1,14 @@
+
+# try with finally in python
+
+try:
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter second number: "))
+    result = num1 / num2
+
+except Exception as e:
+    print("An error occurred:", e)
+
+
+finally:
+    print("Execution completed.") # finally block always executes because it is used for cleanup actions
