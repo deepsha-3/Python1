f611105c3ececeddc3ae07d79be8dbddd0b2af0d
deepsha-3 <itsmedeepsha375@gmail.com>
2025-12-06 21:57:04 +0545

    new code added

---
+++ 13_Advance_Python/match_case.py
@@ -0,0 +1,25 @@
+
+
+# Example of match-case statement in Python.
+
+def weekday_name(day_number):
+    match day_number:
+       case 1:
+            return "Sunday"
+       case 2:
+            return "Monday"
+       case 3:
+            return "Tuesday"
+       case 4:
+            return "Wednesday"
+       case 5:
+            return "Thursday"
+       case 6:
+            return "Friday"
+       case 7:
+            return "Saturday"
+       
+
+if __name__ == "__main__":
+    for i in range(1, 9):
+        print(f"Day {i}: {weekday_name(i)}")
