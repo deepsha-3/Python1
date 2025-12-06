0f3ceb09969438d0522d3a09839914e46a90a992
deepsha-3 <itsmedeepsha375@gmail.com>
2025-12-06 21:42:03 +0545

    new file

--- Advance_Python/enumerate.py
+++ Advance_Python/enumerate.py
@@ -2,12 +2,13 @@
 # enumerate function in Python
 
 # The enumerate() function adds a counter to an iterable and returns it as an enumerate object.
-""" 
+ 
 list = ['apple', 'banana', 'grapes', 'pear']
-
+""" 
 index = 0
 for i in list:
     print(f"The item at index {index} is {i}")
     index += 1
 """
 # Using enumerate to achieve the same result
+
