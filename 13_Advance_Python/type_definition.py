a19f9f3551c9839080a742060f0b144a20c44596
deepsha-3 <itsmedeepsha375@gmail.com>
2025-12-06 22:02:21 +0545

    added file

---
+++ 13_Advance_Python/type_definition.py
@@ -0,0 +1,13 @@
+
+# Type definitions in Python allow you to specify the expected data types of variables, function parameters, and return values.
+
+# This helps improve code readability and can catch type-related errors during development.
+
+num : int = 10
+name : str = "Deepsha"
+
+def greet(user_name: str) -> str:
+    return f"Hello, {user_name}!"
+
+print(greet(name))
+
