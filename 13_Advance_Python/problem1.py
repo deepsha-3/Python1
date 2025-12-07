edf10bd25c9f6f65bad2bb00d492a93ac6619196
deepsha-3 <itsmedeepsha375@gmail.com>
2025-12-07 23:15:57 +0545

    file added

--- 13_Advance_Python/problem1.py
+++ 13_Advance_Python/problem1.py
@@ -1,4 +1,2 @@
 
 # Write a program to open three files 1.txt, 2.txt, and 3.txt if any these files are not present, a message without existing the program must be printed prompting the same. 
-#  preseanr a m
-# message without existing the ethe program
