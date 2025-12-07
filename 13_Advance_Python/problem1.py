6b1bef177946d35caedf484e7f2fe3d306ebeed4
deepsha-3 <itsmedeepsha375@gmail.com>
2025-12-07 23:13:12 +0545

    new fle

--- 13_Advance_Python/problem1.py
+++ 13_Advance_Python/problem1.py
@@ -1,3 +1,4 @@
 
-# Write a program to open three files 1.txt, 2.txt, and 3.txt if any these are preseanr a m
+# Write a program to open three files 1.txt, 2.txt, and 3.txt if any these files are not present,
+#  preseanr a m
 # message without existing the ethe program
