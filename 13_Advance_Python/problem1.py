126c644cfb9a89ab7f6f40681aa0c7b6504799b6
deepsha-3 <itsmedeepsha375@gmail.com>
2025-12-09 20:22:16 +0545

    new file

--- 13_Advance_Python/problem1.py
+++ 13_Advance_Python/problem1.py
@@ -1,6 +1,7 @@
 
 # Write a program to open three files 1.txt, 2.txt, and 3.txt if any these files are not present, a message without existing the program must be printed prompting the same. 
 
+try:
 with open("1.txt", "r") as f:
     print(f.read())
 
@@ -9,3 +10,4 @@ with open("2.txt", "r") as f:
 
 with open("3.txt", "r") as f:
     print(f.read())
+
