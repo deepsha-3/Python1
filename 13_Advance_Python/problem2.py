824c77f912d9fc13a2ff009f9ea6fd6d94d39ecd
deepsha-3 <itsmedeepsha375@gmail.com>
2025-12-11 22:57:11 +0545

    new file

--- 13_Advance_Python/problem2.py
+++ 13_Advance_Python/problem2.py
@@ -5,4 +5,4 @@ list = [10, 20, 30, 40, 50, 60, 70, 80, 90]
 
 for index, value in enumerate(list):
     if index == 2 or index == 4 or index == 6:
-       
+        print(f"The element at index {index} is {value}")
