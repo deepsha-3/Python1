de79a695e5a592475f7d6523ae69d0d6e437ee00
deepsha-3 <itsmedeepsha375@gmail.com>
2025-12-11 23:00:37 +0545

    codE

--- 13_Advance_Python/problem2.py
+++ 13_Advance_Python/problem2.py
@@ -7,3 +7,5 @@ list = [10, 20, 30, 40, 50, 60, 70, 80, 90]
 for index, value in enumerate(list):
     if index == 2 or index == 4 or index == 6:
         print(f"The element at index {index} is {value}")
+
+        # enumrate checks the index not the element itself
