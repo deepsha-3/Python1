f2d5964f831b74c050baaa0b1dc975c4403f5e7d
deepsha-3 <itsmedeepsha375@gmail.com>
2025-12-12 22:46:39 +0545

    new code

--- 13_Advance_Python/problem3.py
+++ 13_Advance_Python/problem3.py
@@ -2,3 +2,4 @@
 # Write a list comprehension to print a list which contains the multiplication table of a user entered number. 
 
 num = int(input("Enter a number: "))
+table = [num * i for i in range(1, 11)]
