d8c9eb0aaa3b2e17311377a0947aafaffca6fb36
deepsha-3 <itsmedeepsha375@gmail.com>
2025-12-13 23:21:16 +0545

    new file

--- 13_Advance_Python/problem4.py
+++ 13_Advance_Python/problem4.py
@@ -1,2 +1,2 @@
 
-# write a program to display a/b where a and b are integers. If b = 0 display infinite by 
+# write a program to display a/b where a and b are integers. If b = 0 display infinite by handling the 'ZeroDivisionError'.
