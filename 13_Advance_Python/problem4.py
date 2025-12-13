ff48bc2de15c5ea3583dfcbf6d22daa4f57ed704
deepsha-3 <itsmedeepsha375@gmail.com>
2025-12-13 23:23:13 +0545

    new file

--- 13_Advance_Python/problem4.py
+++ 13_Advance_Python/problem4.py
@@ -1,2 +1,6 @@
 
 # write a program to display a/b where a and b are integers. If b = 0 display infinite by handling the 'ZeroDivisionError'.
+
+try: 
+    a = int (input("Enter numerator (a): "))
+    b = int (input("Enter denominator (b): "))
