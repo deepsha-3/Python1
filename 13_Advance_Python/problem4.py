5084bd36659f2485c6bc45287c472b4bd1e520a5
deepsha-3 <itsmedeepsha375@gmail.com>
2025-12-14 21:38:53 +0545

    new file added

--- 13_Advance_Python/problem4.py
+++ 13_Advance_Python/problem4.py
@@ -2,8 +2,9 @@
 # write a program to display a/b where a and b are integers. If b = 0 display infinite by handling the 'ZeroDivisionError'.
 
 try: 
-    a = int (input("Enter numerator (a): "))
-    b = int (input("Enter denominator (b): "))
+    a = int (input("Enter a first number (a): "))
+    b = int (input("Enter second number (b): "))
+    print("Result of a/b is:", a/b)
 
 except ZeroDivisionError:
     print("infinite")
