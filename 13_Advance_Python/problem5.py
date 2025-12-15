f58fcbd6e2418408539301a06f856ec66a748225
deepsha-3 <itsmedeepsha375@gmail.com>
2025-12-15 23:56:53 +0545

    new code

--- 13_Advance_Python/problem5.py
+++ 13_Advance_Python/problem5.py
@@ -1,7 +1,6 @@
 
 # Store the multiplication tables genereted in problem 3 in a file named Tables.txt.
 
-
 num = int(input("Enter a number: "))
 table = [num * i for i in range(1, 11)]
 print(f"Multiplication table of {num}: {table}")
