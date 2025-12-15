f9dc90e8813eb50dfe41d817b7615331f059897e
deepsha-3 <itsmedeepsha375@gmail.com>
2025-12-15 23:59:41 +0545

    new code

--- 13_Advance_Python/problem2.py
+++ 13_Advance_Python/problem2.py
@@ -2,7 +2,7 @@
 # Write a program to print third, fifth and seventh element from a list using enumerate function.
 
 list = [10, 20, 30, 40, 50, 60, 70, 80, 90]
-     # here 30 is 3rd element, 50 is 5th element and 70 is 7th element
+       # here 30 is 3rd element, 50 is 5th element and 70 is 7th element
 
 for index, value in enumerate(list):
     if index == 2 or index == 4 or index == 6:
