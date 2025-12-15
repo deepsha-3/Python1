4b8eeaaf4bd7dcfe5037ad6b091bf98bb6698664
deepsha-3 <itsmedeepsha375@gmail.com>
2025-12-15 23:57:35 +0545

    new file

--- 13_Advance_Python/problem5.py
+++ 13_Advance_Python/problem5.py
@@ -5,5 +5,6 @@ num = int(input("Enter a number: "))
 table = [num * i for i in range(1, 11)]
 print(f"Multiplication table of {num}: {table}")
 
+
 with open("Tables.txt", "w") as file:
     file.write(f"Multiplication table of {num}: {table}\n")
